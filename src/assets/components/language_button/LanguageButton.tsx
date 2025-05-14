6a55eeee187c91ae3acbe1534b0620a44b5d57f1
Yakup <yakup.senel33@gmail.com>
2025-05-14 16:31:05 +0200

    first commit

--- src/assets/components/language_button/LanguageButton.tsx
+++ src/assets/components/language_button/LanguageButton.tsx
@@ -1,17 +1,47 @@
 import { useLanguage } from "../../context/LanguageContext";
+import "./LanguageButton.css";
 
 function LanguageButton() {
-	const { setLanguage } = useLanguage();
+	const { language, setLanguage } = useLanguage();
+
+	const flags = {
+		fr: "src/assets/images/language_button/france_flag.png",
+		en: "src/assets/images/language_button/england_flag.png",
+	};
 
 	return (
-		<>
-			<button type="button" onClick={() => setLanguage("fr")}>
-				FR
-			</button>
-			<button type="button" onClick={() => setLanguage("en")}>
-				EN
-			</button>
-		</>
+		<div className="lang_menu">
+			<div className="selected_lang">
+				<img
+					src={flags[language]}
+					alt={`flag for  ${language}`}
+					className="selected_flag"
+				/>
+				{language === "fr" ? "FR" : "EN"}
+			</div>
+			<ul>
+				<li>
+					{" "}
+					<button type="button" onClick={() => setLanguage("fr")}>
+						<img
+							src="src\assets\images\language_button\france_flag.png"
+							alt=""
+						/>
+						French
+					</button>
+				</li>
+				<li>
+					{" "}
+					<button type="button" onClick={() => setLanguage("en")}>
+						<img
+							src="src\assets\images\language_button\england_flag.png"
+							alt=""
+						/>
+						English
+					</button>
+				</li>
+			</ul>
+		</div>
 	);
 }
 
