be5041b3898f97dde3221fcb831f8fa6a2346aec
Yakup <yakup.senel33@gmail.com>
2025-05-26 15:14:04 +0200

    re orgnaisation of folders

--- src/assets/components/header/languageButton/LanguageButton.tsx
+++ src/assets/components/header/languageButton/LanguageButton.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import "./LanguageButton.css";
 import "../../../../i18n.tsx";
 import engFlag from "../../../images/language_button/england_flag.png";
-import frFlag from "../..//images/language_button/france_flag.png";
+import frFlag from "../../../images/language_button/france_flag.png";
 import LanguageButtonModal from "../languageButtonModal/LanguageButtonModal.tsx";
 
 function LanguageButton() {
