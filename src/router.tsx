75f7f2ac345225ead15655f0ce598edb1d2cec6f
Yakup <yakup.senel33@gmail.com>
2026-05-21 17:15:10 +0200

    added basename

--- src/router.tsx
+++ src/router.tsx
@@ -4,27 +4,30 @@ import "./pages/home/Home.css";
 import Experiences from "./pages/experiences/Experiences.tsx";
 import "./pages/experiences/Experiences.css";
 import Projects from "./pages/projects/Projects.tsx";
-import "./pages/home/Home.css";
 import Skills from "./pages/skills/Skills.tsx";
-import "./pages/home/Home.css";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <Home />,
+		},
+		{
+			path: "/experiences",
+			element: <Experiences />,
+		},
+		{
+			path: "/projects",
+			element: <Projects />,
+		},
+		{
+			path: "/skills",
+			element: <Skills />,
+		},
+	],
 	{
-		path: "/",
-		element: <Home />,
+		basename: "/Portfolio",
 	},
-	{
-		path: "/experiences",
-		element: <Experiences />,
-	},
-	{
-		path: "/projects",
-		element: <Projects />,
-	},
-	{
-		path: "/skills",
-		element: <Skills />,
-	},
-]);
+);
 
 export default router;
